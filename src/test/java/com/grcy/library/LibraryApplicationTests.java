82006151b871cd1bfe3ac82f2f2f686ac0b83b9f
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2021-02-09 21:52:42 +0100

    Add first 2 REST tests for GET and POST

--- src/test/java/com/grcy/library/LibraryApplicationTests.java
+++ src/test/java/com/grcy/library/LibraryApplicationTests.java
@@ -1,24 +1,60 @@
 package com.grcy.library;
 
 import com.grcy.library.dao.BookRepository;
+import com.grcy.library.model.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryApplicationTests {
+
+    @LocalServerPort
+    private int localPort;
 
     @Autowired
     BookRepository bookRepository;
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
     void testAllRowsAreReadFromH2() {
         final int noRows = 8;
         int elements = bookRepository.findAll().size();
 
-        assertThat(elements).as("Should have 8 elements in DB").isEqualTo(noRows);
+        assertThat(elements).as("Should have %d elements in DB", noRows).isEqualTo(noRows);
+    }
+
+    @Test
+    void testAllBooksOfAndrzejPilipiuk() {
+        final int noRows = 3;
+        int elements = bookRepository.findBooksByAuthor("Andrzej Pilipiuk").size();
+
+        assertThat(elements).as("Should have %d elements in DB", noRows).isEqualTo(noRows);
+    }
+
+    @Test
+    void testGetAllBooksWithRest() {
+        Book[] actual = restTemplate.getForObject("http://localhost:" + localPort + "/library/books/", Book[].class);
+        assertThat(actual.length).as("Should return list")
+                .isEqualTo(bookRepository.findAll().size());
+    }
+
+    @Test
+    void testPostBookWithRest() {
+        Book book = new Book();
+        book.setTitle("Test title");
+        book.setAuthor("Test author");
+        Book actual = restTemplate.postForObject("http://localhost:" + localPort + "/library/books/", book, Book.class);
+        assertThat(actual.getId()).as("Should return list")
+                .isEqualTo(9);
     }
 
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,12 +6,14 @@ CREATE TABLE books (
   author VARCHAR(250) NOT NULL
 );
 
-INSERT INTO books (author, title) VALUES
-  ('Andrzej Pilipiuk', 'Czarownik Iwanow'),
-  ('Andrzej Pilipiuk', 'Trucizna'),
-  ('Andrzej Pilipiuk', 'Norweski Dziennik'),
-  ('Robert Ludlum', 'Tożsamość Bournea'),
-  ('Robert Ludlum', 'Krucjata Bournea'),
-  ('Robert Ludlum', 'Ultimatum Bournea'),
-  ('Andrzej Ziemiański', 'Achaja'),
-  ('Andrzej Ziemiański', 'Toy Wars');
+CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 9 INCREMENT BY 1;
+
+INSERT INTO books (id, author, title) VALUES
+  (1, 'Andrzej Pilipiuk', 'Czarownik Iwanow'),
+  (2, 'Andrzej Pilipiuk', 'Trucizna'),
+  (3, 'Andrzej Pilipiuk', 'Norweski Dziennik'),
+  (4, 'Robert Ludlum', 'Tożsamość Bournea'),
+  (5, 'Robert Ludlum', 'Krucjata Bournea'),
+  (6, 'Robert Ludlum', 'Ultimatum Bournea'),
+  (7, 'Andrzej Ziemiański', 'Achaja'),
+  (8, 'Andrzej Ziemiański', 'Toy Wars');
