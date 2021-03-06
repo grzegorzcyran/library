061cf5c42e9dd0f216ae6d618868245b0d74dc7f
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2021-03-06 14:25:39 +0100

    Enhance Lombok on Book

--- src/main/java/com/grcy/library/model/Book.java
+++ src/main/java/com/grcy/library/model/Book.java
@@ -1,5 +1,6 @@
 package com.grcy.library.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class Book {
 
     @Id
